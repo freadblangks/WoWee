dbf3038560d0ea74635b7998058cd10244df58a4
Kelsi <kelsihates2fa@gmail.com>
2026-02-09 01:11:49 -0800

    Fix action bar keyboard shortcuts not working
    Added ImGuiWindowFlags_NoInputs to action bar window so it doesn't capture
    keyboard input. Previously, hovering over or clicking the action bar would
    set WantCaptureKeyboard=true, blocking the 1-9,0,-,= hotkeys from working.

--- src/ui/game_screen.cpp
+++ src/ui/game_screen.cpp
@@ -2324,7 +2324,7 @@ void GameScreen::renderActionBar(game::GameHandler& gameHandler) {
 
     ImGuiWindowFlags flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
                              ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar |
-                             ImGuiWindowFlags_NoScrollbar;
+                             ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoInputs;
 
     ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 4.0f);
     ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.05f, 0.05f, 0.05f, 0.9f));
